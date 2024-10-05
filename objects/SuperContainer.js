301c9693c30625e806f9993e03b91a68e11b3cdc
Dmytro Lysenko <demansn@gmail.com>
2024-10-05 11:16:51 +0300

    Fixed SuperContainer

--- objects/SuperContainer.js
+++ objects/SuperContainer.js
@@ -1,13 +1,13 @@
 import {container} from "pixi.js";
-import {mather} from "./mather.js";
+import {Mather} from "./Mather.js";
 
-export class supercontainer extends container {
-    static resourcesgetter = (name) => {};
-    static stylesgetter = (name) => {};
+export class SuperContainer extends container {
+    static resourceGetter = (name) => {};
+    static stylesGetter = (name) => {};
 
     constructor() {
         super();
 
-        this.create = new mather(this, {get: supercontainer.resourcesgetter}, {get: supercontainer.stylesgetter});
+        this.create = new Mather(this, {get: SuperContainer.resourceGetter}, {get: SuperContainer.stylesGetter});
     }
 }

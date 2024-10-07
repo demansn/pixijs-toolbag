80ed547de9903898f1cad5b515223fbf7ce2efd2
Dmytro Lysenko <demansn@gmail.com>
2024-10-07 14:47:28 +0300

    Added  screenSize

--- objects/SuperContainer.js
+++ objects/SuperContainer.js
@@ -4,10 +4,11 @@ import {Mather} from "./Mather.js";
 export class SuperContainer extends Container {
     static resourceGetter = (name) => {};
     static stylesGetter = (name) => {};
+    static screenSize = {width: 0, height: 0};
 
     constructor() {
         super();
 
-        this.create = new Mather(this, {get: SuperContainer.resourceGetter}, {get: SuperContainer.stylesGetter});
+        this.create = new Mather(this, {get: SuperContainer.resourceGetter}, {get: SuperContainer.stylesGetter}, SuperContainer.screenSize);
     }
 }
